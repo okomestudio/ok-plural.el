0729c88d1cc4e4aeb5745643591714eb2b221cf3
Taro Sato <okomestudio@gmail.com>
2024-09-03 13:06:19 -0700

    Create ok-plural.el

---
+++ ok-plural.el
@@ -0,0 +1,10 @@
+;;; ok-plural.el --- plural for Okome Studio  -*- lexical-binding: t -*-
+;;; Code:
+
+(require 'plural)
+
+(defun ok-plural-pluralize (noun)
+  (plural-pluralize noun))
+
+(provide 'ok-plural)
+;;; ok-plural.el ends here
