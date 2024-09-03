f7e2688053825920b6ba3f496a6fc97a93e7d4d3
Taro Sato <okomestudio@gmail.com>
2024-09-03 13:40:35 -0700

    Update ok-plural.el

--- ok-plural.el
+++ ok-plural.el
@@ -1,4 +1,14 @@
 ;;; ok-plural.el --- plural for Okome Studio  -*- lexical-binding: t -*-
+;;
+;; Copyright (C) 2024 Taro Sato
+;;
+;; Author: Taro Sato <okomestudio@gmail.com>
+;; URL: https://github.com/okomestudio/ok-plural.el
+;; Version: 0.1
+;; Keywords: convenience, spelling
+;; Package-Requires: ((emacs "29.1"))
+;;
+;;; Commentary:
 ;;; Code:
 
 (require 'plural)
