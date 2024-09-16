f623542bb41254c6885f0eefd70524991ec532ec
Taro Sato <okomestudio@gmail.com>
2024-09-16 01:50:46 -0700

    Add spell checking to avoid generating words that do not exist

--- ok-plural.el
+++ ok-plural.el
@@ -1,10 +1,19 @@
 ;;; ok-plural.el --- plural for Okome Studio  -*- lexical-binding: t -*-
 ;;; Code:
 
+(require 'ispell)
 (require 'plural)
 
 (defun ok-plural-pluralize (noun)
-  (plural-pluralize noun))
+  "Pluralize given NOUN.
+Pluralization is performed using heuristics. If spell
+checking (with `ispell') determines that the plural form doesn't
+exist, the function returns nil."
+  (let* ((pluralized (plural-pluralize noun))
+         (ispell-filter nil)
+         (spell-checked (ispell--run-on-word pluralized)))
+    (if (eq spell-checked t)
+        pluralized)))
 
 (provide 'ok-plural)
 ;;; ok-plural.el ends here
